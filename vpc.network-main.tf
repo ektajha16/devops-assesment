5321686a9bc7e147625c14899564230b4cc71103
ektajha16 <hit.ektajha@gmail.com>
2022-05-04 23:26:09 +0530

    Update vpc.network-main.tf

--- vpc.network-main.tf
+++ vpc.network-main.tf
@@ -60,3 +60,39 @@ resource aws_subnet public {
     }
 
 }
+
+/* SG creation */
+
+####create sg group prod-web-servers  for port 80 and 443 (Q-A)
+
+resource "aws_security_group" "prod-web-servers-sg" {
+  name        = "prod-web-servers-sg"
+  description = "security group for terraform"
+  vpc_id      = aws_vpc.this_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+/* EC2 creation */
+
+resource "aws_instance" "prod-web-server-1" {
+  ami                    = "ami-0c293f3f676ec4f90"
+  count                  = 1
+  key_name               = "terraform"
+  instance_type          = "t3.large"
+  vpc_security_group_ids = [aws_security_group.prod-web-servers-sg.id]
+  subnet_id              = aws_subnet.private.id
+  tags= {
+    Name = "prod-web-servers-2"
+ }
+}
